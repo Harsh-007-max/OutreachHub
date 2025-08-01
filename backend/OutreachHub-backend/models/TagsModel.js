98b666d15a04f6d0ba5d2166d1e1cda5180ff19b
Harsh <harsh.bhalodiya@kevit.io>
2025-08-01 10:40:11 +0530

    this commit removes unwanted code which is no longer needed

--- backend/OutreachHub-backend/models/TagsModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const tagsSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
-});
-module.exports = mongoose.model("Tag",tagsSchema );
